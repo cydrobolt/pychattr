b2b49989666640d68b69a22107893d735f41576a
Chaoyi Zha <summermontreal@gmail.com>
2014-08-26 11:19:34 -0400

    don't reconnect on kick

--- client/pychattr.js
+++ client/pychattr.js
@@ -1,6 +1,6 @@
 /*
 * PyChattr JavaScript Client Library 
-* [c] Copyright 2014 Chaoyi Zha
+* [c] Copyright 2014 Chaoyi Zha and DogeBoosting
 * </>ed w/ <3 in Philly in '14
 * Authors: Chaoyi Zha <summermontreal@gmail.com>
 * Dependencies: socket.io, jQuery 1.x
@@ -17,7 +17,7 @@ function tsend(message) {
 	socket.send({"data": message, "token": token});	
 }
 $(document).ready(function(){
-    var socket = io.connect('http://' + domain + ':' + port + '/pychattr');
+    var socket = io.connect('http://' + domain + ':' + port + '/pychattr', {reconnect: false});
     socket.on('auth', function(msg) {
         $('#log').append('<p>Received: ' + msg + '</p>');
         token = window.prompt('Please insert your token to authenticate.');
@@ -30,9 +30,10 @@ $(document).ready(function(){
 		}
 			
     });
-    socket.on('disconnected', function(msg) {
-        $('#log').append('<p>Disconnected by server.</p>');
+    socket.on('kick', function(msg) {
+        $('#log').append('<p>Disconnected by server: '+msg+'</p>');
         socket.disconnect();
+        console.log('kicked');
     });
     $('#pm').click(function () {
 		var topm = window.prompt('Who would you like to PM');

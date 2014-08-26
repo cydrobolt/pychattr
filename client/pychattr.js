ced0445f9598b45d8ff4598930238d47e48b4360
Chaoyi Zha <summermontreal@gmail.com>
2014-08-26 11:07:07 -0400

    keep up to date with server-rendered client

--- client/pychattr.js
+++ client/pychattr.js
@@ -1,24 +1,44 @@
 /*
 * PyChattr JavaScript Client Library 
-* [c] Copyright 2014 Chaoyi Zha and DogeBoosting
+* [c] Copyright 2014 Chaoyi Zha
 * </>ed w/ <3 in Philly in '14
 * Authors: Chaoyi Zha <summermontreal@gmail.com>
-* Dependencies: socket.io.min.js
+* Dependencies: socket.io, jQuery 1.x
 */
 
 var domain = 'localhost';
 var port = 5000;
+var token;
 
+function temit(event, message) {
+	socket.emit(event, {"data": message, "token": token});	
+}
+function tsend(message) {
+	socket.send({"data": message, "token": token});	
+}
 $(document).ready(function(){
     var socket = io.connect('http://' + domain + ':' + port + '/pychattr');
     socket.on('auth', function(msg) {
         $('#log').append('<p>Received: ' + msg + '</p>');
+        token = window.prompt('Please insert your token to authenticate.');
+        socket.emit('auth', token);
     });
     socket.on('message', function(msg) {
         $('#log').append('<p>Received: ' + msg + '</p>');
+        if (msg.tojoin && msg.joined) {
+			$('#log').append('<p>PM Request from ' + msg.joined + ' to '+ msg.tojoin +'</p>');
+		}
+			
     });
-    $('form#broadcast').submit(function(event) {
-        socket.emit('my broadcast event', {data: $('#broadcast_data').val()});
-        return false;
+    socket.on('disconnected', function(msg) {
+        $('#log').append('<p>Disconnected by server.</p>');
+        socket.disconnect();
     });
+    $('#pm').click(function () {
+		var topm = window.prompt('Who would you like to PM');
+		socket.emit('pmuser', topm);
+		console.log('PMing '+topm);
+	});
 });
+
+	
